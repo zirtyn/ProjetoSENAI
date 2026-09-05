77a04cf3513b967448b7d69b8c4ca0f53ee516ca
nath <nathypereirapecanha@gmail.com>
2026-09-04 21:45:26 -0300

    fiz mais coisa
    conectando front e back: o inicio
    adicionei footer tambem e organizei o css

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-//dados//
+//esse banquinho de dados vai sair jaja//
 const data = [
     {
         title: "lorem",
@@ -50,37 +50,49 @@ window.addEventListener("load", () => {
 });
 
 
+// ----------requisitando os dados de produtos à API------------
+
+async function searchProduct() {
+    const response = await fetch(
+        "http://127.0.0.1:8000/admin"
+    );
+
+    const product = await response.json();
+
+    console.log(product);
+}
+
+searchProduct();
+
+//esse banquinho de dados vai embora jaja tbm
 const productList = [
     {
-        name: "sla1",
-        price: 1244,
+        name: "produto1",
+        price: 1240.00,
         image: "imagens/product_images/notebook.png"
     },
-
     {
-        name: "sla2",
-        price: 1244,
+        name: "produto2",
+        price: 1229.99,
         image: "imagens/product_images/notebook.png"
     },
-
     {
-        name: "sla3",
-        price: 99399,
+        name: "produto3",
+        price: 999.99,
         image: "imagens/product_images/notebook.png"
     }
 ];
 
-
+// -------------representando os produtos no html----------------
+// dentro de <div id="product">, cria uma <div class="currentProduct"> e os demais dados a cada item recebido
 let productArea = document.getElementById("product")
-
 productList.forEach(product => {
-
     let card = document.createElement("div");
 
     card.className = "currentProduct";
 
     card.innerHTML = `
-            <img src=${product.image} alt="notebook">
+            <img src=${product.image} alt="Imagem">
             <h3>${product.name}</h3>
             <div class="actions">
             <p>R$${product.price}</p>
